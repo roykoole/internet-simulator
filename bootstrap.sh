a318b70e22a2361f6d7423a3236ef36e171ca844
Roy Koole <roykoole@gmail.com>
2025-07-26 10:52:53 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,41 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-echo "[?] Do you want to run the setup (ip.sh) or cleanup (cleanup.sh)?"
-echo "    Type 'setup' to run ip.sh or 'cleanup' to run cleanup.sh."
-read -r USER_CHOICE
+echo "[+] Installing required packages..."
+apk update
+apk add --no-cache nano bash iproute2 iptables dnsmasq wget
 
-if [ "$USER_CHOICE" = "setup" ]; then
+echo "[+] Downloading ip.sh..."
+wget -O /root/ip.sh https://raw.githubusercontent.com/roykoole/internet-simulator/main/ip.sh
+chmod +x /root/ip.sh
 
-    echo "[+] Installing required packages..."
-    apk update
-    apk add --no-cache nano bash iproute2 iptables dnsmasq wget
+echo "[+] Running ip.sh..."
+bash /root/ip.sh
 
-    echo "[+] Downloading ip.sh..."
-    wget -O /root/ip.sh https://raw.githubusercontent.com/roykoole/internet-simulator/main/ip.sh
-    chmod +x /root/ip.sh
+echo "[+] Setting up ip.sh to auto-run at boot..."
+cp /root/ip.sh /etc/local.d/ipstart
+chmod +x /etc/local.d/ipstart
+rc-update add local default
 
-    echo "[+] Running ip.sh..."
-    bash /root/ip.sh
-
-    echo "[+] Setting up ip.sh to auto-run at boot..."
-    cp /root/ip.sh /etc/local.d/ipstart
-    chmod +x /etc/local.d/ipstart
-    rc-update add local default
-
-    echo "[✓] Setup complete. Rebooting in 10 seconds..."
-    sleep 10
-    reboot
-elif [ "$USER_CHOICE" = "cleanup" ]; then
-  echo "[+] Downloading cleanup.sh..."
-  wget -O /root/cleanup.sh https://raw.githubusercontent.com/roykoole/internet-simulator/main/cleanup.sh
-  chmod +x /root/cleanup.sh
-
-  echo "[+] Running cleanup.sh..."
-  bash /root/cleanup.sh
-  echo "[✓] Cleanup complete."
-else
-  echo "[!] Invalid choice. Exiting."
-  exit 1
-fi
+echo "[✓] Setup complete. Rebooting in 10 seconds..."
+sleep 10
+reboot
